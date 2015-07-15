09b2f65d517b078c75498f6f754d1333d9000d1e
taynovai <taynovai88@gmail.com>
2015-07-15 19:41:33 +0500

    Пробуем дальше

--- sympy-test/1/s1.py
+++ sympy-test/1/s1.py
@@ -1,30 +1,34 @@
+# -*- coding: utf-8 -*-
+
 import sympy
 from sympy import *
 import math
 
-def task1 ():
-	x = symbols('x')
-	expression = -log(x)/log(10) + log((x + 8)/(x - 1))/log(10)
+'''
+-log(-x)/log(5) + log(x**2 - 12)/log(5)
+-log(x)/log(10) + log((x + 8)/(x - 1))/log(10)
+'''
+
+'''
+Функция принимает символьное представления выражения, 
+решает уравнение выражение = 0 
+	- не ловит исключения
+'''
+def SolveStringEquation ( stringExpression ):
+	expression = sympify ( stringExpression )
 	equation = Eq(expression, 0)
+
+	x = symbols('x')
 	solution = solve ( equation, x )
+
 	return solution
 
-def task2 ():
-	x = symbols('x')
-	expression = -log(-x)/log(5) + log(x**2 - 12)/log(5)
-	equation = Eq(expression, 0)
-	solution = solve ( equation, x )
-	return solution
-
-solution1 = task1()
+equation1 = "-log(-x)/log(5) + log(x**2 - 12)/log(5)"
+solution1 = SolveStringEquation(equation1)
 print solution1
 
-#x1,x2 = solution1[0], solution1[1]
-#a = -math.log(x1)/math.log(10) + math.log((x1 + 8)/(x1 - 1))/math.log(10)
-#b = -math.log(x2)/math.log(10) + math.log((x2 + 8)/(x2 - 1))/math.log(10)
-#print a, b
-
-solution2 = task2()
+equation2 = "-log(x)/log(10) + log((x + 8)/(x - 1))/log(10)"
+solution2 = SolveStringEquation(equation2)
 print solution2
 
 #x1,x2 = solution2[0], solution2[1]
